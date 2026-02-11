082f7218b2d8607b903f211bd1fb495cf0dc5d62
mshahidbhatti <phdfellows20@gmail.com>
2026-02-11 13:47:58 +0500

    second class with BSE

---
+++ FA25BSEC/Theory/Class 3/FirstApp.java
@@ -0,0 +1,14 @@
+// class name use TitleCase  
+// method or variable names are written in Camel case  firstMethod
+import java.lang.*;   // this package is included implicitly
+
+public class FirstApp{
+
+	// this main is entry point of java applications
+	public static void main(String args[]){
+		
+		System.out.println("First App");
+	
+	}
+
+}
